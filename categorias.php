8496d44383f2b6a2001606ad47d796a73a084fde
Ezequias Lopes <ezequias@hotmail.com.br>
2018-04-11 10:27:34 -0500

    @saiuke
    Sistema de ícones do FontAwesome atualizado para versão 5.0.
    Arquivos da versão anterior removidos.
    Adicionado botão na página de categorias.

--- categorias.php
+++ categorias.php
@@ -1,81 +1,41 @@
-<!DOCTYPE html>
-<html>
-
+<?php
+    include "conexao.php";
+    $buscaCategorias = mysqli_query($conexao, "SELECT * FROM tb_categoria;"); 
+?>
+<!doctype html>
+<html lang="pt-br">
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <title>Página Modelo</title>
     <meta charset="utf-8">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
     <!--  Meu CSS  -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
     <!-- Menu -->
-    <nav class="navbar navbar-inverse">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                </form>
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </nav>
-    <!--  Galeria de filmes  -->
+    <?php
+        include "menu.html";
+    ?>
+    <!-- Listagem de categorias -->
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <form class="cadastroFilmes form-horizontal">
-                    <fieldset>
-                        <div class="novoPreco" style="display:none;">
-                            <br>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <label for="pacote_1" class="control-label col-sm-2"></label>
-                            <div class="col-sm-8">
-                                <div class="btn btn-success criarPreco col-sm-4 col-sm-offset-4">
-                                    <i class="fa fa-money fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Cadastrar preços
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
+                <div class="listaCategorias">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-info">Terror</button>
+                        <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -202,5 +162,4 @@
     <!-- Js personalizado -->
     <script src="js/script.js"></script>
 </body>
-
 </html>
--- listarFilmes.php
+++ listarFilmes.php
@@ -14,7 +14,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
     <!-- Plyr CSS -->
     <link rel="stylesheet" type="text/css" href="css/plyr.css">
     <!--  Meu CSS  -->
